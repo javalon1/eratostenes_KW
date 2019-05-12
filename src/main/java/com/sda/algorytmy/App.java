445602975e1dfbf39a510a327009b2760e3418be
Karolina Wanat <carolinawanat@gmail.com>
2019-05-12 14:46:07 +0100

    init repo

---
+++ src/main/java/com/sda/algorytmy/App.java
@@ -0,0 +1,24 @@
+package com.sda.algorytmy;
+
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) {
+        int[] tab = new int[49];
+        int z =0;
+        for (int i = 2; i <51 ; i++) {
+            tab[z++] = i;
+        }
+
+        for (int i = 0; i < tab.length ; i++) {
+            if (tab[i] %2 ==0){
+
+            }
+
+        }
+
+
+    }
+}
